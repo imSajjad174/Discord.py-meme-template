c8d6ad2fd6c7097e33c039b51daf7a32bc26f5d4
SAJJAD <85294721+imSajjad174@users.noreply.github.com>
2023-06-21 18:10:37 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+import requests
+import random
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+
+intents = discord.Intents.default()
+intents.members = True
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+
+
+@bot.command()
+async def memes(ctx):
+    """Generates a random meme."""
+    try:
+        response = requests.get('https://api.imgflip.com/get_memes')
+        data = response.json()
+        memes = data['data']['memes']
+        random_meme = random.choice(memes)
+        meme_url = random_meme['url']
+        
+        embed = discord.Embed()
+        embed.set_image(url=meme_url)
+        
+        await ctx.send(embed=embed)
+        
+    except Exception as e:
+        await ctx.send(f'An error occurred: {str(e)}')
+        
+  
+  
+  
+  
+  
+@bot.event
+async def on_slash_command_error(ctx, ex):
+    await ctx.send(str(ex))
+
+bot.run(TOKEN)
